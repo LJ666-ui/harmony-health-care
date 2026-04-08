4da024e22862935c1183ef2c630363206370ed7b
LJ666-ui <asdf857857123@163.com>
2026-04-08 10:08:07 +0800

    后端基础搭建

---
+++ src/main/java/com/medical/MedicalApplication.java
@@ -0,0 +1,14 @@
+package com.medical;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 关键：扫描 Mapper 接口
+@MapperScan("com.medical.mapper")
+@SpringBootApplication
+public class MedicalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalApplication.class, args);
+    }
+}

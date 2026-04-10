49b3829788cc2061cb17893e2f2263d4e6100f95
ysj67384 <673846448@qq.com>
2026-04-10 09:19:12 +0800

    feat: 新增康复计划模块测试

--- src/main/java/com/medical/MedicalApplication.java
+++ src/main/java/com/medical/MedicalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 关键：扫描 Mapper 接口
-@MapperScan("com.medical.mapper")
+@MapperScan("com.medical.test.mapper")
 @SpringBootApplication
 public class MedicalApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/medical/test/services/RehabPlanService.java
@@ -0,0 +1,7 @@
+package com.medical.test.services; // 修正包名
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.medical.test.entity.RehabPlan;
+
+public interface RehabPlanService extends IService<RehabPlan> {
+}

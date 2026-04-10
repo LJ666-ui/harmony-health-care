bac55503638f4e80ab7c839da0453ad230ea8822
xiaozhi0421 <lyz15934978521@163.com>
2026-04-10 14:35:11 +0800

    lyz

---
+++ src/main/java/com/medical/test_lyz/services/HealthFoodService.java
@@ -0,0 +1,7 @@
+package com.medical.test_lyz.services; // 修正包名
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.medical.test_lyz.entity.HealthFood;
+
+public interface HealthFoodService extends IService<HealthFood> {
+}

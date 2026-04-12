c86b694821da211687a86ca8c1bebe1aeeee8c12
LYK-214 <liyingkui12@gmail.com>
2026-04-12 19:51:32 +0800

    lyk

--- src/main/java/com/example/medical/service/HealthArticleService.java
+++ src/main/java/com/example/medical/service/HealthArticleService.java
@@ -1,7 +1,16 @@
 package com.example.medical.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.medical.common.PageResult;
 import com.example.medical.entity.HealthArticle;
 
+import java.util.List;
+
 public interface HealthArticleService extends IService<HealthArticle> {
+    PageResult<HealthArticle> pageQuery(int page, int pageSize, String title, String parentCategoryName, String childCategoryName);
+    HealthArticle getDetailById(Long id);
+    boolean addArticle(HealthArticle healthArticle);
+    boolean updateArticle(HealthArticle healthArticle);
+    boolean deleteArticle(Long id);
+    List<HealthArticle> listByCategoryNames(String title, String parentCategoryName, String childCategoryName);
 }

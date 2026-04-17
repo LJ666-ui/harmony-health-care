c33f59c962693a21189903a548acbe3840ac0380
LJ666-ui <asdf857857123@163.com>
2026-04-17 19:39:30 +0800

    1
    1

--- sql/medical_health.sql
+++ sql/medical_health.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021aa3739ee739c6e8d2f8d0e66a5e0ee583c0f9bd735dcf2f2cf32de9ce5c2c
-size 60819940
+oid sha256:f6eefb24f3c022fac5daa2a0256614ef1dea2663e892c3252d09ec7d26a23680
+size 59493439

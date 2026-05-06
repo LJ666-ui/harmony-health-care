833bd3eaec6473190c5fae700f85838e7bb7219f
ysj67384 <673846448@qq.com>
2026-05-06 13:48:21 +0800

    yyy

--- sql/medical_health.sql
+++ sql/medical_health.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:266fa3ef0b45403f9ded3fc123e519ab3105eb0c78ef4249ed7ad966ccdf2c8a
-size 59578647
+oid sha256:93228cfc36c846cb1f7faef5e46fdeacd67a27d7ae1321a4e1340981448d65ea
+size 59580613

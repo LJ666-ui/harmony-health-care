afba702c5ad52fac1d9bdb18a292c079f43d984c
ysj67384 <673846448@qq.com>
2026-05-15 18:43:58 +0800

    yyy

--- fix-doctor-realname.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `doctor` ADD COLUMN `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '真实姓名' AFTER `user_id`;
--- generate_admin_password.sql
+++
@@ -1,9 +0,0 @@
--- 生成admin用户的正确密码hash
--- admin123 的 bcrypt hash (使用BCryptPasswordEncoder生成)
--- 以下是一个有效的bcrypt hash，对应明文密码 "admin123"
-
--- 更新admin表的密码
-UPDATE admin SET password = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGfJ3q0Q5dZ5dZ5dZ5dZ5dZ5dZ5dZ5dZ5dZ5dZ5d' WHERE username = 'admin';
-
--- 查看更新后的结果
-SELECT id, username, password, real_name, role, status FROM admin WHERE username = 'admin';
